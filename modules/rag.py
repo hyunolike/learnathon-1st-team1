32144cbdfb6dee77aca3ff5ee0c75ed4e3518243
honeyuheony <kwh1019_@naver.com>
2025-03-29 14:43:22 +0900

    feat: docker fastmcp 초안 구ㅇ

--- modules/rag.py
+++ modules/rag.py
@@ -36,10 +36,7 @@ class DocumentEmbedder:
         )
         
         self.vectorstore = Chroma(
-            # persist_directory=persist_directory,
-            # persist_directory="./db/chroma",  # 쓰기 가능한 경로로 변경
-            persist_directory=None,
-            # persist_directory="/tmp/chroma_db",  # ✅ 맥/리눅스 공용 임시 쓰기 디렉토리
+            persist_directory=persist_directory,
             embedding_function=self.embeddings,
             collection_name=collection_name
         )
